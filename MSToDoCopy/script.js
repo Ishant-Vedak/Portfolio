4956299f236c060a05a763c6e7d813d7bae2272b
Ishant <vedakishant@gmail.com>
2025-07-11 23:51:48 -0700

    V8 - Got updating date

--- MSToDoCopy/script.js
+++ MSToDoCopy/script.js
@@ -1,3 +1,10 @@
+//goofing around
+
+const main = document.getElementById('main')
+
+main.innerHTML = "working so far"
+
+//Notes Stuff
 //render notes
 
 //load notes
@@ -6,3 +13,20 @@
 
 //remove notes
 
+
+//Update Date and time
+
+function updateTime() {
+    const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const monthsArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const currentDateObj = new Date();
+const currentDay = weekDays[currentDateObj.getDay()];
+const currentDate = currentDateObj.getDate();
+const currentMonth = monthsArr[currentDateObj.getMonth()];
+
+document.getElementById('day-and-date').textContent = `${currentDay}, ${currentMonth} ${currentDate}`
+}
+
+setInterval(updateTime, 1000)
+
+updateTime()
