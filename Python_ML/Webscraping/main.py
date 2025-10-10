28c9ecbfe2d69c23751e2fd97bfef6ad5f1b1bee
Ishant <vedakishant@gmail.com>
2025-10-09 18:14:39 -0700

    got the simple stuff working for selenium, and added gitignore for this

--- Python_ML/Webscraping/main.py
+++ Python_ML/Webscraping/main.py
@@ -1,9 +1,12 @@
+from creds import USERNAME, PASSWORD
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+'''  .\.venv\Scripts\activate  '''
 
 options = Options()
 options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
@@ -14,9 +17,8 @@ website = "https://google.com"
 driver.get(website)
 
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-time.sleep(10)
-input_element.send_keys("Selenium python" + Keys.ENTER)
-time.sleep(10)
+input_element.send_keys("scholartree.com" + Keys.ENTER)
+time.sleep(30)
 
 driver.quit()
 
