c480444677d8d41fe58ca6421d268196017aa816
Ishant <vedakishant@gmail.com>
2025-10-17 20:29:49 -0700

    finally clicked the website

--- Python_ML/Webscraping/main.py
+++ Python_ML/Webscraping/main.py
@@ -4,10 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 '''  .\.venv\Scripts\activate  '''
-
+duration = 100
 options = Options()
 options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 
@@ -16,9 +18,22 @@ driver = webdriver.Chrome(service=Service(), options=options)
 website = "https://google.com"
 driver.get(website)
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 input_element.send_keys("scholartree.com" + Keys.ENTER)
-time.sleep(30)
+
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.XPATH, "//h3"))
+)
+
+scholarship_website = driver.find_element(By.PARTIAL_LINK_TEXT, "ScholarTree")
+scholarship_website.click()
+
+
+time.sleep(duration)
 
 driver.quit()
 
